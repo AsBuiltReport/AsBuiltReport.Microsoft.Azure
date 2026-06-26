2d572dedebcf4d3a1e8bdd78cf7bcfa0de0f5dc3
Tim Carman <15333667+tpcarman@users.noreply.github.com>
2026-06-26 14:53:59 +1000

    Add vault security properties and health checks to Recovery Services Vault reporter

--- AsBuiltReport.Microsoft.Azure/Src/Private/Report/Get-AbrAzRecoveryServicesVault.ps1
+++ AsBuiltReport.Microsoft.Azure/Src/Private/Report/Get-AbrAzRecoveryServicesVault.ps1
@@ -45,18 +45,42 @@ function Get-AbrAzRecoveryServicesVault {
                         $AzRsvInfo = @()
                         foreach ($AzRsv in $AzRsvs) {
                             $InObj = [Ordered]@{
-                                $LocalizedData.Name = $AzRsv.Name
-                                $LocalizedData.ResourceGroup = $AzRsv.ResourceGroupName
-                                $LocalizedData.Location = $AzLocationLookup."$($AzRsv.Location)"
-                                $LocalizedData.Subscription = "$($AzSubscriptionLookup.(($AzRsv.Id).split('/')[2]))"
-                                $LocalizedData.SubscriptionID = ($AzRsv.Id).split('/')[2]
-                                $LocalizedData.ProvisioningState = $AzRsv.Properties.ProvisioningState
-                                $LocalizedData.PrivateEndpointStateForBackup = Switch ($AzRsv.Properties.PrivateEndpointStateForBackup) {
-                                    $null { $LocalizedData.None }
+                                $LocalizedData.Name                                = $AzRsv.Name
+                                $LocalizedData.ResourceGroup                       = $AzRsv.ResourceGroupName
+                                $LocalizedData.Location                            = $AzLocationLookup."$($AzRsv.Location)"
+                                $LocalizedData.Subscription                        = "$($AzSubscriptionLookup.(($AzRsv.Id).split('/')[2]))"
+                                $LocalizedData.SubscriptionID                      = ($AzRsv.Id).split('/')[2]
+                                $LocalizedData.ProvisioningState                   = $AzRsv.Properties.ProvisioningState
+                                $LocalizedData.StorageRedundancy                   = Switch ($AzRsv.Properties.RedundancySettings.StandardTierStorageRedundancy) {
+                                    $null   { $LocalizedData.None }
+                                    default { $AzRsv.Properties.RedundancySettings.StandardTierStorageRedundancy }
+                                }
+                                $LocalizedData.CrossRegionRestore                  = Switch ($AzRsv.Properties.RedundancySettings.CrossRegionRestore) {
+                                    $null   { $LocalizedData.None }
+                                    default { $AzRsv.Properties.RedundancySettings.CrossRegionRestore }
+                                }
+                                $LocalizedData.SoftDeleteState                     = Switch ($AzRsv.Properties.SoftDeleteSettings.SoftDeleteState) {
+                                    $null   { $LocalizedData.None }
+                                    default { $AzRsv.Properties.SoftDeleteSettings.SoftDeleteState }
+                                }
+                                $LocalizedData.SoftDeleteRetentionDays             = Switch ($AzRsv.Properties.SoftDeleteSettings.SoftDeleteRetentionPeriodInDays) {
+                                    $null   { $LocalizedData.None }
+                                    default { $AzRsv.Properties.SoftDeleteSettings.SoftDeleteRetentionPeriodInDays }
+                                }
+                                $LocalizedData.ImmutabilityState                   = Switch ($AzRsv.Properties.ImmutabilitySettings.ImmutabilityState) {
+                                    $null   { $LocalizedData.None }
+                                    default { $AzRsv.Properties.ImmutabilitySettings.ImmutabilityState }
+                                }
+                                $LocalizedData.PublicNetworkAccess                 = Switch ($AzRsv.Properties.PublicNetworkAccess) {
+                                    $null   { $LocalizedData.None }
+                                    default { $AzRsv.Properties.PublicNetworkAccess }
+                                }
+                                $LocalizedData.PrivateEndpointStateForBackup       = Switch ($AzRsv.Properties.PrivateEndpointStateForBackup) {
+                                    $null   { $LocalizedData.None }
                                     default { $AzRsv.Properties.PrivateEndpointStateForBackup }
                                 }
                                 $LocalizedData.PrivateEndpointStateForSiteRecovery = Switch ($AzRsv.Properties.PrivateEndpointStateForSiteRecovery) {
-                                    $null { $LocalizedData.None }
+                                    $null   { $LocalizedData.None }
                                     default { $AzRsv.Properties.PrivateEndpointStateForSiteRecovery }
                                 }
                             }
@@ -65,16 +89,31 @@ function Get-AbrAzRecoveryServicesVault {
                                 if ($rl) { ($rl | ForEach-Object { "$($_.Name) ($($_.Properties.Level))" }) -join [Environment]::NewLine }
                                 else { $LocalizedData.None }
                             )
+                            $InObj[$LocalizedData.Tags] = $(
+                                if ($AzRsv.Tags -and $AzRsv.Tags.Count -gt 0) {
+                                    ($AzRsv.Tags.GetEnumerator() | ForEach-Object { "$($_.Key): $($_.Value)" }) -join [Environment]::NewLine
+                                } else {
+                                    $LocalizedData.None
+                                }
+                            )
 
                             $AzRsvInfo += [PSCustomObject]$InObj
                         }
 
-                        # Apply health check highlighting
                         if ($Healthcheck.RecoveryServicesVault.ProvisioningState) {
                             $AzRsvInfo | Where-Object { $_.$($LocalizedData.ProvisioningState) -ne 'Succeeded' } | Set-Style -Style Critical -Property $LocalizedData.ProvisioningState
                         }
                         if ($Healthcheck.RecoveryServicesVault.PrivateEndpointStateForBackup) {
                             $AzRsvInfo | Where-Object { $_.$($LocalizedData.PrivateEndpointStateForBackup) -eq $LocalizedData.None } | Set-Style -Style Warning -Property $LocalizedData.PrivateEndpointStateForBackup
+                        }
+                        if ($Healthcheck.RecoveryServicesVault.SoftDeleteEnabled) {
+                            $AzRsvInfo | Where-Object { $_.$($LocalizedData.SoftDeleteState) -eq 'Disabled' } | Set-Style -Style Warning -Property $LocalizedData.SoftDeleteState
+                        }
+                        if ($Healthcheck.RecoveryServicesVault.ImmutabilityEnabled) {
+                            $AzRsvInfo | Where-Object { $_.$($LocalizedData.ImmutabilityState) -eq 'Disabled' } | Set-Style -Style Warning -Property $LocalizedData.ImmutabilityState
+                        }
+                        if ($Healthcheck.RecoveryServicesVault.PublicNetworkAccess) {
+                            $AzRsvInfo | Where-Object { $_.$($LocalizedData.PublicNetworkAccess) -eq 'Enabled' } | Set-Style -Style Warning -Property $LocalizedData.PublicNetworkAccess
                         }
 
                         if ($InfoLevel.RecoveryServicesVault -ge 2) {
@@ -82,8 +121,8 @@ function Get-AbrAzRecoveryServicesVault {
                             foreach ($AzRsv in $AzRsvInfo) {
                                 Section -Style NOTOCHeading5 -ExcludeFromTOC "$($AzRsv.($LocalizedData.Name))" {
                                     $TableParams = @{
-                                        Name = "$($LocalizedData.Heading) - $($AzRsv.($LocalizedData.Name))"
-                                        List = $true
+                                        Name         = "$($LocalizedData.Heading) - $($AzRsv.($LocalizedData.Name))"
+                                        List         = $true
                                         ColumnWidths = 40, 60
                                     }
                                     if ($Report.ShowTableCaptions) {
@@ -96,9 +135,9 @@ function Get-AbrAzRecoveryServicesVault {
                             Paragraph ($LocalizedData.ParagraphSummary -f $AzSubscription.Name)
                             BlankLine
                             $TableParams = @{
-                                Name = "$($LocalizedData.TableHeadings) - $($AzSubscription.Name)"
-                                List = $false
-                                Columns = $LocalizedData.Name, $LocalizedData.ResourceGroup, $LocalizedData.Location
+                                Name         = "$($LocalizedData.TableHeadings) - $($AzSubscription.Name)"
+                                List         = $false
+                                Columns      = $LocalizedData.Name, $LocalizedData.ResourceGroup, $LocalizedData.Location
                                 ColumnWidths = 33, 34, 33
                             }
                             if ($Report.ShowTableCaptions) {
